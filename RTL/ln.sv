e5e1dd741c60d970e0236a20a402f40c7cb00578
Shraddha Sahoo <39151337+shraddha375@users.noreply.github.com>
2026-09-11 19:24:03 +0530

    Update ln.sv

--- RTL/ln.sv
+++ RTL/ln.sv
@@ -1,27 +1,7 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/04/2025 02:30:45 PM
-// Design Name: 
-// Module Name: ln
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module ln(
-    input logic signed [15:0] x_in,
+    input  logic signed [15:0] x_in,
     output logic signed [15:0] ln_out
     );
     
