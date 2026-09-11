3c8bf61d58c829d416c46ac6fea039216f76201e
Shraddha Sahoo <39151337+shraddha375@users.noreply.github.com>
2026-09-11 22:23:31 +0530

    Update sqrt_rom.sv

--- RTL/sqrt_rom.sv
+++ RTL/sqrt_rom.sv
@@ -1,27 +1,7 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/04/2025 04:49:45 PM
-// Design Name: 
-// Module Name: sqrt_rom
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module sqrt_rom(
-    input logic [9:0] addr,
+    input  logic        [9:0] addr,
     output logic signed [15:0] out
     );
     
