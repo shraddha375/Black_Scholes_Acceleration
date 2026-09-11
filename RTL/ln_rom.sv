b4192e1a1057f16d3edebfb6f3e9ac4ecf831a2f
Shraddha Sahoo <39151337+shraddha375@users.noreply.github.com>
2026-09-11 19:24:42 +0530

    Update ln_rom.sv

--- RTL/ln_rom.sv
+++ RTL/ln_rom.sv
@@ -1,27 +1,7 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/04/2025 02:29:00 PM
-// Design Name: 
-// Module Name: ln_rom
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module ln_rom(
-    input logic [9:0] addr,
+    input  logic [9:0]  addr,
     output logic [15:0] out
     );
     
